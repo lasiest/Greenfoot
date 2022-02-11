3e6d11c7e608dedef02ce2ec97b5218443ffbb27
lasiest <valenstiowiyogo@gmail.com>
2022-02-11 22:51:59 +0700

    Mini Blob

--- OOGP_Project/Blob.java
+++ OOGP_Project/Blob.java
@@ -8,10 +8,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Blob extends player
 {
-    /**
-     * Act - do whatever the Blob wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    
+    public Blob()
+    {
+        GreenfootImage image = getImage();
+        image.scale(image.getWidth() - 15, image.getHeight() - 15);
+        setImage(image);
+    }
+    
     public void act()
     {
         move(10);
--- OOGP_Project/Blob2.java
+++ OOGP_Project/Blob2.java
@@ -8,10 +8,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Blob2 extends player
 {
-    /**
-     * Act - do whatever the Blob2 wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public Blob2()
+    {
+        GreenfootImage image = getImage();
+        image.scale(image.getWidth() - 15, image.getHeight() - 15);
+        setImage(image);
+    }
     public void act()
     {
         move(-10);
